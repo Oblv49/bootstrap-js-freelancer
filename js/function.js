388c2fbcb2be81838a8658f23c5047760be4266c
DavideMinutoli <noprivacyworld@gmail.com>
2023-07-10 14:44:35 +0200

    bonus remove code after use

--- js/function.js
+++ js/function.js
@@ -1,17 +1,33 @@
 //this function need typo of work and hours requestd end return cost of work 
-function calculateCost (workRequest, hoursRequest) {
-    
-    if(workRequest === "Beck-End") cost = hoursRequest * 20.50;
-    if(workRequest === "Front-End") cost = hoursRequest * 15.30;
-    if(workRequest === "Project Analisys") cost = hoursRequest * 33.60;
+function calculateCost(workRequest, hoursRequest) {
 
-    return cost;
+    if (workRequest === "Beck-End 20.50€/h") cost = hoursRequest * 20.50;
+    if (workRequest === "Front-End 15.30€/h") cost = hoursRequest * 15.30;
+    if (workRequest === "Project Analisys 33.60€/h") cost = hoursRequest * 33.60;
+
+    return parseInt(cost);
 }
 
 
 function addCoupon(coupons, coupon, cost) {
+
     if (coupons.includes(coupon)) {
-      cost = cost - (cost / 100 * 25);
+        
+        cost = cost - (cost / 100 * 25);
+        
+        // Search index of coupon in the array
+        const index = coupons.indexOf(coupon);
+
+        if (index !== -1) {
+            //Remove coupon code 
+            coupons.splice(index, 1);
+        }
     }
+    
     return cost;
-  }
+
+}
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-    /*
-        Quando l’utente fa click sul bottone “send” del form, il sito deve calcolare l’ammontare del preventivo per le ore di lavoro richieste.
-        Il prezzo orario per una commissione varia in questo modo:
-        - se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.5€ l’ora
-        - se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.3€ l’ora
-        - se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.6€
-        L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti:
-        - YHDNU32
-        - JANJC63
-        - PWKCN25
-        - SJDPO96
-        - POCIE24
-        Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul prezzo finale. 
-        Se il codice inserito non è valido, il sito deve informare l’utente che il codice non è valido e il prezzo finale viene calcolato senza applicare sconti.
-        Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” (con 2 decimali e il simbolo dell’euro) in un apposito tag HTML appena sotto
-        il bottone send.
-        Alcuni consigli
-        - Ricordatevi che se non state bene attenti, Javascript vi fa le magie con i tipi
-        - Ricordatevi che il form ha un comportamento “strano” quando fate click sul bottone Send che è di tipo submit (type=submit).
-        Bonus:
-        Quando l’utente clicca sul pulsante “Send”, se il codice promozionale inserito non è valido, facciamo diventare quest’ultimo di colore rosso.
-        Se il codice inserito è valido, dopo aver calcolato il prezzo scontato, eliminare quel codice dall’elenco dei codici sconto disponibili, in modo 
-        che non sia più utilizzabile.
+/*
+    Quando l’utente fa click sul bottone “send” del form, il sito deve calcolare l’ammontare del preventivo per le ore di lavoro richieste.
+    Il prezzo orario per una commissione varia in questo modo:
+    - se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.5€ l’ora
+    - se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.3€ l’ora
+    - se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.6€
+    L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti:
+    - YHDNU32
+    - JANJC63
+    - PWKCN25
+    - SJDPO96
+    - POCIE24
+    Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul prezzo finale. 
+    Se il codice inserito non è valido, il sito deve informare l’utente che il codice non è valido e il prezzo finale viene calcolato senza applicare sconti.
+    Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” (con 2 decimali e il simbolo dell’euro) in un apposito tag HTML appena sotto
+    il bottone send.
+    Alcuni consigli
+    - Ricordatevi che se non state bene attenti, Javascript vi fa le magie con i tipi
+    - Ricordatevi che il form ha un comportamento “strano” quando fate click sul bottone Send che è di tipo submit (type=submit).
+    Bonus:
+    Quando l’utente clicca sul pulsante “Send”, se il codice promozionale inserito non è valido, facciamo diventare quest’ultimo di colore rosso.
+    Se il codice inserito è valido, dopo aver calcolato il prezzo scontato, eliminare quel codice dall’elenco dei codici sconto disponibili, in modo 
+    che non sia più utilizzabile.
 
-    */
+*/
 //creation array whit discount code
 let coupons = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 //listener on button for form
 function submitForm(event) {
-    
+
     event.preventDefault();
 
     //controll function listener event
@@ -46,28 +46,30 @@ function submitForm(event) {
     let discountCode = document.getElementById("inputDiscountCode").value;
     let messageWork = document.getElementById("floatingTextarea2").value;
 
-    //controllo se i dati inseriti sono corretti
+    //controll form data imput
     if (typeof firstUserName === 'string' && typeof lastUserName === 'string' && Number.isInteger(hoursRequested) && workRequest.selectedIndex != 0) {
-        
-        //operation
+
+        //make visible the preventive tag
         document.getElementById("preventiveContainer").classList.remove("d-none");
+
+        //operation
         cost = calculateCost(typeWorkRequest, hoursRequested);
-        document.getElementById("nameApplicant").innerHTML = "Cara/o Sig.ra/Sig ";
+        document.getElementById("applicant").innerHTML = "Cara/o Sig.ra/Sig ";
         document.getElementById("greenNameApplicant").innerHTML = firstUserName + " " + lastUserName;
         document.getElementById("thanksYou").innerHTML = "Grazie per avermi contattato. Ecco il suo preventivo:";
-        document.getElementById("yourEmail").innerHTML ="Your Email: " + emailUser;
-        document.getElementById("yourHoursRequested").innerHTML ="Hours Requested for work: " + hoursRequested + "h " + "total";
-        document.getElementById("typeOfWork").innerHTML ="Type Of Work Requested: " + typeWorkRequest;
+        document.getElementById("yourEmail").innerHTML = "Your Email: " + emailUser;
+        document.getElementById("yourHoursRequested").innerHTML = "Hours Requested for work: " + hoursRequested + "h " + "total";
+        document.getElementById("typeOfWork").innerHTML = "Type Of Work Requested: " + typeWorkRequest;
         document.getElementById("discoutCode").innerHTML = "Your Discount Code: " + discountCode;
         document.getElementById("typeRateContainer").innerHTML = typeWorkRequest;
         cost = addCoupon(coupons, discountCode, cost);
         document.getElementById("priceContainer").innerHTML = cost.toFixed(2) + "€";
-       
 
 
     } else {
 
-        alert("Inserisci i dati corretti!");
+        //alert error
+        alert("Inserisci tutti i dati in modo corretto!");
 
     }
 }
